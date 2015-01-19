78f7c3e8092050a18a0cd49664f4ee4f62f90ff3
Roman Ananyev <aTastyCookie@users.noreply.github.com>
2015-01-19 11:25:54 +0300

    Delete faepec.php

--- admin/sandbox/faepec.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$sb_name="FaEpec";
-$sb_dbuser="sbfaepec";
-$sb_dbname="sbfaepec";
-$sb_dbpassword="TXr0nA1Z";
-$sb_installedVersion="";
-$sb_installedVersion="Epec-20140905-150240";
-$sb_installedVersion="Epec-20140905-154144";
